b674e70d6a74b4dcccdeeb994043e1d1994d550c
PronoyKumar Mondal <pronoy15-14744@diu.edu.bd>
2023-03-13 13:58:06 +0600

    py basic string practice

--- string_practice.py
+++ string_practice.py
@@ -37,4 +37,21 @@ print(k.lower())
 #Strip()-Removes space from the beginning or the end
 k=" Hello, World!"
 print(k.strip())
+#replace() method replaces a string with another string
+k="Hello, World!"
+print(k.replace("H","J"))
+#split() method splits the string into substrings if it finds instances of the separator
+print(k.split())
+#format() method to insert numbers into strings
+age =22
+k="My age is {}"
+print(k.format(age))
 
+result=3.48
+income=0
+k="My name is pronoy. My age is {}. My cgpa is {}. My monthly income is {}."
+print(k.format(age,result,income))
+k="My name is pronoy. My age is {2}. My cgpa is {0}. My monthly income is {1}."
+print(k.format(result,income,age))
+k="My name is \"Pronoy\""
+print(k)
